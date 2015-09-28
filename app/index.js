4e87d1f90f1811be887d7a976a8f8ecc2113db21
Federico Maffei <federico.maffei@gmail.com>
2015-09-28 12:43:51 +0100

    added ability to ask for name and description

--- app/index.js
+++ app/index.js
@@ -1,8 +1,31 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var name,
+    description;
 
 module.exports = generators.Base.extend({
+
+    prompting: function () {
+        var done = this.async();
+        this.prompt({
+            type    : 'input',
+            name    : 'name',
+            message : 'Please enter the name of the project',
+            default : this.appname
+        }, function (answers) {
+            name = answers.name;
+            this.prompt({
+                type    : 'input',
+                name    : 'description',
+                message : 'Please enter the description of the project',
+                default : 'Description'
+            }, function (answers) {
+                description = answers.description;
+                done();
+            }.bind(this));
+        }.bind(this));
+    },
     writing: function () {
         this.fs.copyTpl(
             this.templatePath('index.js'),
@@ -10,7 +33,8 @@ module.exports = generators.Base.extend({
         );
         this.fs.copyTpl(
             this.templatePath('package.json'),
-            this.destinationPath('package.json')
+            this.destinationPath('package.json'),
+            { name: name, description: description }
         );
         this.fs.copyTpl(
             this.templatePath('./src/views/index.jade'),
